4cc65a90fa3ab7489085ca176191906c22a31c49
cody <olfdag@gmail.com>
2017-09-24 21:08:07 -0500

    Defined vars

--- Tearsheet.py
+++ Tearsheet.py
@@ -1,5 +1,9 @@
-files = "\\leelac4\\lac_common\\TEARSHEET\\"
 month = input("Enter the month in 'MM' format. ")
+day = 1
+files = "\\leelac4\\lac_common\\TEARSHEET\\*{}{}_*.pdf"
+pfiles = "\\leelac4\\lac_common\\TEARSHEET\\*{}{}_P*.pdf"
+
+
 
 
 print(files)
